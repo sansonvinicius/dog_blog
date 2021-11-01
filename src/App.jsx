78c0931ff3ef355c3e4db951bb883b0eba381151
Vinicius Henrique Sanson <sansonvinicius@192.168.2.6>
2021-11-01 14:30:21 -0300

    adicionada barra de navegação na aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import './assets/css/base/base.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './componentes/Cabecalho'
 
 function App() {
 
   return (
     <Router>
+      <Cabecalho />
       <Switch>
         <Route exact path='/'>
           <Home />
