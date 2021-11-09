df45e584c2d07a83b6530d90758aa64f1158b6b5
Vinicius Henrique Sanson <sansonvinicius@192.168.2.4>
2021-11-09 15:27:59 -0300

    implementação das rotas para subcategorias dos posts

--- src/paginas/Categoria.jsx
+++ src/paginas/Categoria.jsx
@@ -2,12 +2,13 @@ import { React, useState, useEffect} from 'react'
 import '../assets/css/blog.css'
 import ListaCategorias from '../componentes/ListaCategorias'
 import ListaPost from '../componentes/ListaPost'
-import { Route, useParams, useRouteMatch} from 'react-router-dom'
+import { Route, useParams, useRouteMatch, Link, Switch} from 'react-router-dom'
 import { busca } from '../api/api'
+import SubCategoria from '../paginas/SubCategoria'
 
 const Categoria = () => {
     const { id } = useParams()
-    const { path } = useRouteMatch()
+    const { url, path } = useRouteMatch()
     const [ subcategorias, setSubcategorias] = useState([])
 
     useEffect(() => {
@@ -23,9 +24,28 @@ const Categoria = () => {
         </div>
 
         <ListaCategorias />
-        <Route exact path={`${path}`}> 
-            <ListaPost url={ `/posts?categoria=${id}`}/>
-        </Route>
+        <ul className="lista-categorias container flex">
+            {
+                subcategorias.map((subcategoria) =>(
+                    <li className={`lista-categorias__categoria 
+                    lista-categorias__categoria--${id}`}  
+                    key={subcategoria}>
+                    <Link to={`${url}/${subcategoria}`}>
+                        {subcategoria}
+                    </Link>
+                    </li>
+                ))
+               
+            }
+        </ul>
+        <Switch>
+            <Route exact path={`${path}`}> 
+                <ListaPost url={ `/posts?categoria=${id}`}/>
+            </Route>
+            <Route path={`${path}/:subcategoria`}>
+                <SubCategoria />
+            </Route>
+        </Switch>
         </>
     )
 }
